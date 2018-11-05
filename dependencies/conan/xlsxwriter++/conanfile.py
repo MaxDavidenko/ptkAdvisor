5e5c9487bcd2f5f1c63d2077b4c94c6d4e55a099
MaxDavidenko <max.d.davidenko@gmail.com>
2018-11-04 20:05:02 -0800

    refactored.

--- dependencies/conan/xlsxwriter++/conanfile.py
+++ dependencies/conan/xlsxwriter++/conanfile.py
@@ -35,6 +35,7 @@ class XlntConan(ConanFile):
         self.copy("*.h", dst="include", src="libxlsxwriterpp/libxlsxwriter/include")
         self.copy("*.dll", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
      
